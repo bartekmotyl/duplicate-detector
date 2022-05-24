61885bd40f1073d12f20d60606a726a8076e4929
bartekmotyl <29923773+bartekmotyl@users.noreply.github.com>
2022-05-24 22:31:47 +0200

    sort

--- src/moveDuplicates.ts
+++ src/moveDuplicates.ts
@@ -7,22 +7,27 @@ export type DuplicateFinderConfig = {
     pattern: string 
 }
 
-function findDuplicates(config: DuplicateFinderConfig) {
+function findDuplicates(config: DuplicateFinderConfig): string[] {
     const existingFs = new FileSet(config.existing)
     const incomingFs = new FileSet(config.incoming)
     const twoSetDuplicates = existingFs.getTwoSetDuplicates(incomingFs) 
+    const lines: string[] = []
     twoSetDuplicates.forEach((duplicate, _hash) => {
         duplicate.incomingFiles.forEach(incomingFile => {
             let replaced = incomingFile.replaceVariables(config.pattern)
             replaced = incomingFs.replaceVariables(replaced)
-            console.log(`${replaced}`)
+            lines.push(`${replaced}`)
         })
     })    
+    lines.sort()
+    return lines
+    
 }
 
 const configJson = fs.readFileSync('moveDuplicates.config.json', 'utf8')
 const config = JSON.parse(configJson)
 
 
-findDuplicates(config)
+const lines = findDuplicates(config)
+lines.forEach(line => console.log(line))
 
